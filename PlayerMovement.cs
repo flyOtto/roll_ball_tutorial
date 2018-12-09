da73fc3ce80677126c5d9642fe582e9e311aad77
sa_marjanah <31473448+flyOtto@users.noreply.github.com>
2018-12-09 12:52:17 +0200

    More test

---
+++ PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    // This is a reference to the Rigidbody component called "rb"
+    public Rigidbody rb;
+	
+	// Messing with Physics
+	void FixedUpdate () {
+
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+		
+	}
+}
