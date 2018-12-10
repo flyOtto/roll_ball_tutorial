05d46793d0c6cdd4adcee99e976251b5375d08a5
sa_marjanah <31473448+flyOtto@users.noreply.github.com>
2018-12-10 14:07:39 +0200

    Bashing into Stuff
    Working on it.

---
+++ PlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    void onCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.name == "Obstacle");
+        {
+            Debug.Log("We hit Obstacle!");
+        }
+    }
+
+}
