464a7c4ea3c0103ed12c366e63726cf26a2aca83
sa_marjanah <31473448+flyOtto@users.noreply.github.com>
2018-12-10 14:03:37 +0200

    Camera movement

---
+++ FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    //links to our player to get information for the camera to follow
+    public Transform player;
+    public Vector3 offset;
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = player.position + offset;
+	}
+}
